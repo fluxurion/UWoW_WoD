c08ea1cd151bd52a381ea9a69dba9cd9d59d7e2e
Subv <subv2112@gmail.com>
2014-07-02 18:38:22 -0500

    Fixed an uninitialized value that made the MapUpdater worker threads exit on launch

--- src/server/game/Maps/MapUpdater.cpp
+++ src/server/game/Maps/MapUpdater.cpp
@@ -44,7 +44,7 @@ class MapUpdateRequest
         }
 };
 
-MapUpdater::MapUpdater(): pending_requests(0) { }
+MapUpdater::MapUpdater() : pending_requests(0), _cancelationToken(false) {}
 
 MapUpdater::~MapUpdater()
 {
@@ -61,6 +61,8 @@ void MapUpdater::activate(size_t num_threads)
 
 void MapUpdater::deactivate()
 {
+    _cancelationToken = true;
+
     wait();
 
     _queue.Cancel();
