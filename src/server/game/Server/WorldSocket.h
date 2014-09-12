ccd35873f17929e4356931cc76dc39d01c63ee44
Shauren <shauren.trinity@gmail.com>
2014-09-12 20:25:13 +0200

    Core/WorldSocket: Refactored client packet header check
    Conflicts:
    	src/server/game/Server/WorldSocket.h

--- src/server/game/Server/WorldSocket.h
+++ src/server/game/Server/WorldSocket.h
@@ -39,7 +39,8 @@ struct ClientPktHeader
     uint16 size;
     uint32 cmd;
 
-    bool IsValid() const { return size >= 4 && size < 10240 && cmd < OPCODE_COUNT; }
+    bool IsValidSize() const { return size >= 4 && size < 10240; }
+    bool IsValidOpcode() const { return cmd < OPCODE_COUNT; }
 };
 
 #pragma pack(pop)
