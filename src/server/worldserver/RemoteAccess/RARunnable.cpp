jairoe05/tc_duplicate

Replaced Threading and SFMT access related code with std::thread and boost TSS
Note: The remote access thread is currently broken due to unknown ACE fail (will be replaced at some point anyways..)

src/server/worldserver/RemoteAccess/RARunnable.cpp
@@ -33,29 +33,22 @@
 
 #include "RASocket.h"
 
-RARunnable::RARunnable()
+
+void RemoteAccessThread()
 {
-    ACE_Reactor_Impl* imp;
+    ACE_Reactor_Impl* imp = nullptr;
 
 #if defined (ACE_HAS_EVENT_POLL) || defined (ACE_HAS_DEV_POLL)
     imp = new ACE_Dev_Poll_Reactor();
-    imp->max_notify_iterations (128);
-    imp->restart (1);
+    imp->max_notify_iterations(128);
+    imp->restart(1);
 #else
     imp = new ACE_TP_Reactor();
-    imp->max_notify_iterations (128);
+    imp->max_notify_iterations(128);
 #endif
 
-    m_Reactor = new ACE_Reactor (imp, 1);
-}
+    ACE_Reactor* reactor = new ACE_Reactor(imp, 1);
 
-RARunnable::~RARunnable()
-{
-    delete m_Reactor;
-}
-
-void RARunnable::run()
-{
     if (!sConfigMgr->GetBoolDefault("Ra.Enable", false))
         return;
 
@@ -65,7 +58,7 @@ void RARunnable::run()
     std::string stringIp = sConfigMgr->GetStringDefault("Ra.IP", "0.0.0.0");
     ACE_INET_Addr listenAddress(raPort, stringIp.c_str());
 
-    if (acceptor.open(listenAddress, m_Reactor) == -1)
+    if (acceptor.open(listenAddress, reactor) == -1)
     {
         TC_LOG_ERROR("server.worldserver", "Trinity RA can not bind to port %d on %s", raPort, stringIp.c_str());
         return;
@@ -76,9 +69,12 @@ void RARunnable::run()
     while (!World::IsStopped())
     {
         ACE_Time_Value interval(0, 100000);
-        if (m_Reactor->run_reactor_event_loop(interval) == -1)
+        if (reactor->run_reactor_event_loop(interval) == -1)
             break;
     }
+
+    delete imp;
+    delete reactor;
 
     TC_LOG_DEBUG("server.worldserver", "Trinity RA thread exiting");
 }

fluxurion/UWoW_WoD

Conflicts:
	dep/SFMT/SFMT.h
	src/server/shared/Database/MySQLThreading.h
	src/server/shared/Threading/Threading.cpp
	src/server/shared/Threading/Threading.h
	src/server/shared/Utilities/Util.cpp
	src/server/shared/Utilities/Util.h
	src/server/worldserver/CommandLine/CliRunnable.h
	src/server/worldserver/Master.cpp
	src/server/worldserver/RemoteAccess/RARunnable.cpp
	src/server/worldserver/RemoteAccess/RARunnable.h
	src/server/worldserver/TCSoap/TCSoap.cpp
	src/server/worldserver/TCSoap/TCSoap.h
	src/server/worldserver/WorldThread/WorldRunnable.h

src/server/worldserver/RemoteAccess/RARunnable.cpp
@@ -33,29 +33,22 @@
 
 #include "RASocket.h"
 
-RARunnable::RARunnable()
+
+void RemoteAccessThread()
 {
-    ACE_Reactor_Impl* imp = NULL;
+    ACE_Reactor_Impl* imp = nullptr;
 
 #if defined (ACE_HAS_EVENT_POLL) || defined (ACE_HAS_DEV_POLL)
     imp = new ACE_Dev_Poll_Reactor();
-    imp->max_notify_iterations (128);
-    imp->restart (1);
+    imp->max_notify_iterations(128);
+    imp->restart(1);
 #else
     imp = new ACE_TP_Reactor();
-    imp->max_notify_iterations (128);
+    imp->max_notify_iterations(128);
 #endif
 
-    m_Reactor = new ACE_Reactor (imp, 1);
-}
+    ACE_Reactor* reactor = new ACE_Reactor(imp, 1);
 
-RARunnable::~RARunnable()
-{
-    delete m_Reactor;
-}
-
-void RARunnable::run()
-{
     if (!ConfigMgr::GetBoolDefault("Ra.Enable", false))
         return;
 
@@ -65,7 +58,7 @@ void RARunnable::run()
     std::string stringip = ConfigMgr::GetStringDefault("Ra.IP", "0.0.0.0");
     ACE_INET_Addr listen_addr(raport, stringip.c_str());
 
-    if (acceptor.open(listen_addr, m_Reactor) == -1)
+    if (acceptor.open(listen_addr, reactor) == -1)
     {
         sLog->outError(LOG_FILTER_WORLDSERVER, "Trinity RA can not bind to port %d on %s", raport, stringip.c_str());
         return;
@@ -78,9 +71,12 @@ void RARunnable::run()
         // don't be too smart to move this outside the loop
         // the run_reactor_event_loop will modify interval
         ACE_Time_Value interval(0, 100000);
-        if (m_Reactor->run_reactor_event_loop(interval) == -1)
+        if (reactor->run_reactor_event_loop(interval) == -1)
             break;
     }
+
+    delete imp;
+    delete reactor;
 
     sLog->outDebug(LOG_FILTER_WORLDSERVER, "Trinity RA thread exiting");
 }