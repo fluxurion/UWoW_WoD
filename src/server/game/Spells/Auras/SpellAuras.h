jairoe05/tc_duplicate

Buildsystem/MSVC: Warning fixes
C4800 'type' : forcing value to bool 'true' or 'false' (performance warning)
C4127 conditional expression is constant

src/server/game/Spells/Auras/SpellAuras.h
@@ -67,9 +67,9 @@ class AuraApplication
         uint8 GetSlot() const { return _slot; }
         uint8 GetFlags() const { return _flags; }
         uint8 GetEffectMask() const { return _flags & (AFLAG_EFF_INDEX_0 | AFLAG_EFF_INDEX_1 | AFLAG_EFF_INDEX_2); }
-        bool HasEffect(uint8 effect) const { ASSERT(effect < MAX_SPELL_EFFECTS);  return _flags & (1<<effect); }
-        bool IsPositive() const { return _flags & AFLAG_POSITIVE; }
-        bool IsSelfcast() const { return _flags & AFLAG_CASTER; }
+        bool HasEffect(uint8 effect) const { ASSERT(effect < MAX_SPELL_EFFECTS);  return (_flags & (1 << effect)) != 0; }
+        bool IsPositive() const { return (_flags & AFLAG_POSITIVE) != 0; }
+        bool IsSelfcast() const { return (_flags & AFLAG_CASTER) != 0; }
         uint8 GetEffectsToApply() const { return _effectsToApply; }
 
         void SetRemoveMode(AuraRemoveMode mode) { _removeMode = mode; }

fluxurion/UWoW_WoD

Conflicts:
	src/server/game/AI/CoreAI/UnitAI.cpp
	src/server/game/AI/CreatureAI.cpp
	src/server/game/AI/SmartScripts/SmartScript.cpp
	src/server/game/Battlegrounds/Arena.cpp
	src/server/game/Battlegrounds/Battleground.h
	src/server/game/Battlegrounds/BattlegroundMgr.cpp
	src/server/game/Battlegrounds/Zones/BattlegroundRV.cpp
	src/server/game/Calendar/CalendarMgr.h
	src/server/game/Chat/Channels/Channel.h
	src/server/game/Conditions/ConditionMgr.cpp
	src/server/game/Conditions/DisableMgr.cpp
	src/server/game/DungeonFinding/LFGMgr.h
	src/server/game/Entities/Creature/Creature.cpp
	src/server/game/Entities/Creature/Creature.h
	src/server/game/Entities/GameObject/GameObject.h
	src/server/game/Entities/Object/Object.h
	src/server/game/Entities/Player/Player.cpp
	src/server/game/Entities/Player/Player.h
	src/server/game/Entities/Unit/Unit.cpp
	src/server/game/Entities/Unit/Unit.h
	src/server/game/Globals/ObjectMgr.h
	src/server/game/Groups/GroupMgr.cpp
	src/server/game/Handlers/BattleGroundHandler.cpp
	src/server/game/Handlers/CharacterHandler.cpp
	src/server/game/Handlers/GroupHandler.cpp
	src/server/game/Handlers/NPCHandler.cpp
	src/server/game/Handlers/PetHandler.cpp
	src/server/game/Maps/Map.cpp
	src/server/game/Movement/PathGenerator.cpp
	src/server/game/Movement/Spline/MoveSplineFlag.h
	src/server/game/Quests/QuestDef.cpp
	src/server/game/Quests/QuestDef.h
	src/server/game/Spells/Auras/SpellAuras.h
	src/server/game/Spells/Spell.cpp
	src/server/game/Spells/Spell.h
	src/server/game/Tools/CharacterDatabaseCleaner.cpp
	src/server/game/World/World.cpp
	src/server/scripts/Commands/cs_debug.cpp
	src/server/scripts/Commands/cs_misc.cpp
	src/server/scripts/EasternKingdoms/MagistersTerrace/instance_magisters_terrace.cpp
	src/server/scripts/EasternKingdoms/SunwellPlateau/boss_kiljaeden.cpp
	src/server/scripts/Northrend/ChamberOfAspects/ObsidianSanctum/obsidian_sanctum.cpp
	src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_grand_champions.cpp
	src/server/sc

src/server/game/Spells/Auras/SpellAuras.h
@@ -69,9 +69,9 @@ class AuraApplication
         uint8 GetSlot() const { return _slot; }
         uint8 GetFlags() const { return _flags; }
         uint32 GetEffectMask() const { return _effectMask; }
-        bool HasEffect(uint8 effect) const { ASSERT(effect < MAX_SPELL_EFFECTS);  return _effectMask & (1<<effect); }
-        bool IsPositive() const { return _flags & AFLAG_POSITIVE; }
-        bool IsSelfcasted() const { return _flags & AFLAG_CASTER; }
+        bool HasEffect(uint8 effect) const { ASSERT(effect < MAX_SPELL_EFFECTS);  return (_effectMask & (1<<effect)) != 0; }
+        bool IsPositive() const { return (_flags & AFLAG_POSITIVE) != 0; }
+        bool IsSelfcasted() const { return (_flags & AFLAG_CASTER) != 0; }
         uint32 GetEffectsToApply() const { return _effectsToApply; }
 
         void SetRemoveMode(AuraRemoveMode mode) { _removeMode = mode; }