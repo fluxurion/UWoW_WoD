jairoe05/tc_duplicate

Core: Implemented IPC (Inter-process communication) between worldserver and bnetserver using ZeroMQ library.
* Implemented ToonReady and ToonLoggedOut battle.net packets

src/server/scripts/Commands/cs_misc.cpp
@@ -1542,7 +1542,7 @@ public:
 
         // Query the prepared statement for login data
         stmt = LoginDatabase.GetPreparedStatement(LOGIN_SEL_PINFO);
-        stmt->setInt32(0, int32(realmID));
+        stmt->setInt32(0, int32(realmHandle.Index));
         stmt->setUInt32(1, accId);
         PreparedQueryResult result = LoginDatabase.Query(stmt);
 

fluxurion/UWoW_WoD

Conflicts:
	src/server/CMakeLists.txt
	src/server/game/AuctionHouse/AuctionHouseMgr.cpp
	src/server/game/CMakeLists.txt
	src/server/game/Handlers/CharacterHandler.cpp
	src/server/game/Handlers/MiscHandler.cpp
	src/server/game/Server/WorldSession.cpp
	src/server/game/Server/WorldSocket.cpp
	src/server/game/World/World.h
	src/server/scripts/Commands/cs_misc.cpp
	src/server/scripts/Commands/cs_rbac.cpp
	src/server/scripts/Commands/cs_ticket.cpp
	src/server/worldserver/CMakeLists.txt
	src/server/worldserver/Main.cpp
	src/server/worldserver/worldserver.conf.dist

src/server/scripts/Commands/cs_misc.cpp
@@ -1707,7 +1707,7 @@ public:
         std::string lastLogin   = handler->GetTrinityString(LANG_ERROR);
 
         PreparedStatement* stmt = LoginDatabase.GetPreparedStatement(LOGIN_SEL_PINFO);
-        stmt->setInt32(0, int32(realmID));
+        stmt->setInt32(0, int32(realmHandle.IndexlmID));
         stmt->setUInt32(1, accId);
         PreparedQueryResult result = LoginDatabase.Query(stmt);
 