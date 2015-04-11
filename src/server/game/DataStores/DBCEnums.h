klister803/PandaCores548

фикс регена маны у мага

src/server/game/DataStores/DBCEnums.h
@@ -472,6 +472,10 @@ enum SpawnMask
     SPAWNMASK_DUNGEON_HEROIC    = (1 << HEROIC_DIFFICULTY),
     SPAWNMASK_DUNGEON_CHALLENGE = (1 << CHALLENGE_MODE_DIFFICULTY),
     SPAWNMASK_DUNGEON_ALL       = (SPAWNMASK_DUNGEON_NORMAL | SPAWNMASK_DUNGEON_HEROIC | SPAWNMASK_DUNGEON_CHALLENGE),
+    
+    SPAWNMASK_SCENARIO_NORMAL   = (1 << NORMAL_SCENARIO_DIFFICULTY ),
+    SPAWNMASK_SCENARIO_HEROIC   = (1 << HEROIC_SCENARIO_DIFFICULTY ),
+    SPAWNMASK_SCENARIO_ALL      = (SPAWNMASK_SCENARIO_NORMAL | SPAWNMASK_SCENARIO_HEROIC),
 
     SPAWNMASK_RAID_10MAN_NORMAL = (1 << MAN10_DIFFICULTY ),
     SPAWNMASK_RAID_25MAN_NORMAL = (1 << MAN25_DIFFICULTY),

fluxurion/UWoW_WoD

src/server/game/DataStores/DBCEnums.h
@@ -495,6 +495,10 @@ enum SpawnMask
     SPAWNMASK_DUNGEON_HEROIC    = (1 << DIFFICULTY_HEROIC),
     SPAWNMASK_DUNGEON_CHALLENGE = (1 << DIFFICULTY_CHALLENGE),
     SPAWNMASK_DUNGEON_ALL       = (SPAWNMASK_DUNGEON_NORMAL | SPAWNMASK_DUNGEON_HEROIC | SPAWNMASK_DUNGEON_CHALLENGE),
+    
+    SPAWNMASK_SCENARIO_NORMAL   = (1 << DIFFICULTY_N_SCENARIO),
+    SPAWNMASK_SCENARIO_HEROIC   = (1 << DIFFICULTY_HC_SCENARIO),
+    SPAWNMASK_SCENARIO_ALL      = (SPAWNMASK_SCENARIO_NORMAL | SPAWNMASK_SCENARIO_HEROIC),
 
     SPAWNMASK_RAID_10MAN_NORMAL = (1 << DIFFICULTY_10_N ),
     SPAWNMASK_RAID_25MAN_NORMAL = (1 << DIFFICULTY_25_N),