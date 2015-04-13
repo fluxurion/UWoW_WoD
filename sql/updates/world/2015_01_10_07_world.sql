f6f32c778f0ce7f04308cd9dd6a8409a111dabce
Yaki Khadafi <ElSolDolLo@gmail.com>
2015-04-13 18:18:24 +0300

    fix

--- sql/updates/world/2015_01_10_07_world.sql
+++ sql/updates/world/2015_01_10_07_world.sql
@@ -1,30 +1,29 @@
-ALTER TABLE `creature_classlevelstats`
-  DROP `basehp0`,
-  DROP `basehp1`,
-  DROP `basehp2`,
-  DROP `basehp3`,
-  DROP `basehp4`,
-  DROP `basehp5`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp0`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp1`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp2`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp3`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp4`;
+ALTER TABLE `creature_classlevelstats` DROP `basehp5`;
 
 DELETE FROM `creature_classlevelstats` WHERE `level` BETWEEN 101 AND 105;
-INSERT INTO `creature_classlevelstats` (`level`, `class`, `basemana`, `basearmor`, `attackpower`, `rangedattackpower`, `damage_base`, `damage_exp1`, `damage_exp2`, `damage_exp3`, `damage_exp4`, `damage_exp5`, `comment`) VALUES
-(101, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(101, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(101, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(101, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(102, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(102, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(102, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(102, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(103, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(103, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(103, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(103, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(104, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(104, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(104, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(104, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(105, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(105, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(105, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(105, 8, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, '');
+INSERT INTO `creature_classlevelstats` (`level`, `class`, `basemana`, `basearmor`) VALUES
+(101, 1, 1, 1),
+(101, 2, 1, 1),
+(101, 4, 1, 1),
+(101, 8, 1, 1),
+(102, 1, 1, 1),
+(102, 2, 1, 1),
+(102, 4, 1, 1),
+(102, 8, 1, 1),
+(103, 1, 1, 1),
+(103, 2, 1, 1),
+(103, 4, 1, 1),
+(103, 8, 1, 1),
+(104, 1, 1, 1),
+(104, 2, 1, 1),
+(104, 4, 1, 1),
+(104, 8, 1, 1),
+(105, 1, 1, 1),
+(105, 2, 1, 1),
+(105, 4, 1, 1),
+(105, 8, 1, 1);
