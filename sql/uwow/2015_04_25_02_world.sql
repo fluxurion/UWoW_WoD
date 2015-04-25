76f5decc2323d3e05e9d299926e62fae0eee8041
cyberbrest <cyberbrest@cyberbrest.com>
2015-04-25 16:59:58 +0300

    [Spell]: Druid: Moonfire

---
+++ sql/uwow/2015_04_25_02_world.sql
@@ -0,0 +1,3 @@
+-- Druid: Moonfire
+INSERT INTO `spell_trigger_dummy` (`spell_id`, `spell_trigger`, `option`, `target`, `caster`, `targetaura`, `bp0`, `bp1`, `bp2`, `effectmask`, `aura`, `chance`, `group`, `check_spell_id`, `comment`) VALUES
+ ('8921', '164812', '14', '0', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', 'Druid: Moonfire');
