7e9f4910f593db6960a160d103c0645702deede8
cyberbrest <cyberbrest@cyberbrest.com>
2015-04-27 10:53:09 +0300

    [SQL]: lost change.

--- sql/uwow/2015_04_26_03_world_creature_difficulty.sql
+++ sql/uwow/2015_04_26_03_world_creature_difficulty.sql
@@ -3,3 +3,4 @@ ALTER TABLE `creature_template` DROP `faction_H`;
 ALTER TABLE `creature_template` CHANGE `Health_mod` `HealthModifier` FLOAT NOT NULL DEFAULT '1';
 ALTER TABLE `creature_template` CHANGE `Mana_mod` `ManaModifier` FLOAT NOT NULL DEFAULT '1';
 ALTER TABLE `creature_template` CHANGE `WDBVerified` `VerifiedBuild` SMALLINT(6) NULL DEFAULT '1';
+ALTER TABLE `creature_difficulty_stat` CHANGE `Health_mod` `HealthModifier` INT UNSIGNED NOT NULL DEFAULT '1';
