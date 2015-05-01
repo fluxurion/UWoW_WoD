73317e0180e526c6250b84dd02e376c636b7cc7b
cyberbrest <cyberbrest@cyberbrest.com>
2015-05-01 18:41:02 +0300

    [Vmaps]: fix loading for new format 0000 digits for maps.

--- src/server/collision/Maps/MapTree.h
+++ src/server/collision/Maps/MapTree.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -79,7 +79,7 @@ namespace VMAP
             bool LoadMapTile(uint32 tileX, uint32 tileY, VMapManager2* vm);
             void UnloadMapTile(uint32 tileX, uint32 tileY, VMapManager2* vm);
             bool isTiled() const { return iIsTiled; }
-            uint32 numLoadedTiles() const { return iLoadedTiles.size(); }
+            uint32 numLoadedTiles() const { return uint32(iLoadedTiles.size()); }
             void getModelInstances(ModelInstance* &models, uint32 &count);
 
         private:
