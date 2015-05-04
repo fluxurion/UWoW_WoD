392c7f7b597166a96ea49d9b80a0425e1ded6b04
cyberbrest <cyberbrest@cyberbrest.com>
2015-05-04 11:37:03 +0300

    [Transport]: all periods.

---
+++ sql/uwow/2015_05_04_01_world_transportd_WOD.sql
@@ -0,0 +1,37 @@
+-- Undercity, Tirisfal Glades and Grom'gol Base Camp, Stranglethorn Vale ("The Purple Princess")
+UPDATE `transports` SET `period` = '319212' WHERE `entry` = 176495;
+-- Stormwind Harbor and Auberdine, Darkshore ("Ship (The Bravery)")
+UPDATE `transports` SET `entry` = '235785' WHERE `entry` = 235785;
+-- Rut'theran Village, Teldrassil and Auberdine, Darkshore ("The Moonspray")
+UPDATE `transports` SET `name` = '<DEPR> Rut''theran Village, Teldrassil and Auberdine, Darkshore ("The Moonspray")' WHERE `entry` = 176244;
+-- Menethil Harbor, Wetlands and Theramore Isle, Dustwallow Marsh ("The Lady Mehley")
+UPDATE `transports` SET `name` = '<DEPR> Menethil Harbor, Wetlands and Theramore Isle, Dustwallow Marsh ("The Lady Mehley")' WHERE `entry` = 176231;
+-- Orgrimmar, Durotar and Grom'gol Base Camp, Stranglethorn Vale ("The Iron Eagle")
+UPDATE `transports` SET `period` = '244533' WHERE `entry` = 175080;
+-- Orgrimmar, Durotar and Undercity, Tirisfal Glades ("The Thundercaller")
+UPDATE `transports` SET `period` = '255869' WHERE `entry` = 164871;
+-- Steamwheedle Cartel ports, Ratchet and Booty Bay ("The Maiden's Fancy") 20808
+UPDATE `transports` SET `period` = '231236' WHERE `entry` = 20808;  -- old from MOP
+-- The Forgotten Coast, Feralas and Feathermoon Stronghold, Sardor Isle, Feralas ("Feathermoon Ferry") 177233
+UPDATE `transports` SET `name` = '<DEPR> The Forgotten Coast, Feralas and Feathermoon Stronghold, Sardor Isle, Feralas ("Feathermoon Ferry")' WHERE `entry` = 177233;
+-- Valaar's Berth, Azuremyst Isle and Auberdine, Darkshore ("Elune's Blessing")
+UPDATE `transports` SET `period` = '238660' WHERE `entry` = 181646;
+-- Menethil Harbor, Wetlands and Valgarde, Howling Fjord ("Northspear") 181688 Alliance Nord
+UPDATE `transports` SET `period` = '445534' WHERE `entry` = 181688; -- old from MOP
+-- Undercity, Tirisfal Glades and Vengeance Landing, Howling Fjord ("Zeppelin, Horde (Cloudkisser)")
+UPDATE `transports` SET `period` = '214581' WHERE `entry` = 181689;
+-- Orgrimmar, Durotar and Warsong Hold, Borean Tundra ("Zeppelin, Horde (The Mighty Wind)")
+UPDATE `transports` SET `period` = '298833' WHERE `entry` = 186238;
+-- Westguard Keep in Howling Fjord to bombard pirate ("Zeppelin") 186371
+UPDATE `transports` SET `period` = '484211' WHERE `entry` = 186371; -- Quest??
+-- Not Boardable - Cyrcling in Howling Fjord ("Sister Mercy")
+-- Unu'pe, Borean Tundra and Moa'ki Harbor, Dragonblight ("Turtle (Walker of Waves)")
+UPDATE `transports` SET `period` = '445228' WHERE `entry` = 187568;
+-- Moa'ki Harbor and Kamagua ("Turtle (Green Island)") 188511
+UPDATE `transports` SET `period` = '502354' WHERE `entry` = 188511;
+-- Stormwing Harbor and Valiance Keep, Borean Tundra ("The Kraken")
+UPDATE `transports` SET `period` = '271981' WHERE `entry` = 190536;
+-- Not boardable - Horde gunship patrolling above Icecrown ("Orgrim's Hammer")
+-- Not boardable - Alliance gunship patrolling above Icecrown ("The Skybreaker")
+-- Orgrimmar and Thunder Bluff
+UPDATE `transports` SET `period` = '599150' WHERE `entry` = 190549;
