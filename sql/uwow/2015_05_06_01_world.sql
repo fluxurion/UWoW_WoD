d0695efcc1cd2d42e1af365781d447573432219b
cyberbrest <cyberbrest@cyberbrest.com>
2015-05-06 21:29:22 +0300

    [Spell/DK]: Icy Touch & Plague Strike

---
+++ sql/uwow/2015_05_06_01_world.sql
@@ -0,0 +1,3 @@
+INSERT INTO `spell_trigger_dummy` (`spell_id`, `spell_trigger`, `option`, `target`, `caster`, `targetaura`, `bp0`, `bp1`, `bp2`, `effectmask`, `aura`, `chance`, `group`, `check_spell_id`, `comment`) VALUES
+ ('45477', '55095', '14', '0', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', 'DK: Icy Touch'),
+ ('45462', '55078', '14', '0', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', 'DK: Plague Strike');
