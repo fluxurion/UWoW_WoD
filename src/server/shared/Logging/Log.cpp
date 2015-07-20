84dbc34e2f5ea695646346ae874f897ad0e03a10
cyberbrest <cyberbrest@cyberbrest.com>
2015-07-20 15:00:30 +0300

    [Trinity]: Shared/Logs: Improve log performances
    Improve Log::ShouldLog() performances by saving the lowest log level across all loggers and discarding any log with lower level than that.

--- src/server/shared/Logging/Log.cpp
+++ src/server/shared/Logging/Log.cpp
@@ -217,6 +217,9 @@ void Log::CreateLoggerFromConfig(const char* name)
         return;
     }
 
+    if (level < lowestLogLevel)
+         lowestLogLevel = level;
+
     logger.Create(name, LogFilterType(type), level);
     //fprintf(stdout, "Log::CreateLoggerFromConfig: Created Logger %s, Type %u, mask %u\n", name, LogFilterType(type), level); // DEBUG - RemoveMe
 
@@ -340,6 +343,9 @@ bool Log::SetLogLevel(std::string const& name, const char* newLevelc, bool isLog
         if (it == loggers.end())
             return false;
 
+        if (newLevel != LOG_LEVEL_DISABLED && newLevel < lowestLogLevel)
+            lowestLogLevel = newLevel;
+
         it->second.setLogLevel(newLevel);
     }
     else
@@ -355,6 +361,10 @@ bool Log::SetLogLevel(std::string const& name, const char* newLevelc, bool isLog
 
 bool Log::ShouldLog(LogFilterType type, LogLevel level) const
 {
+    // Don't even look for a logger if the LogLevel is lower than lowest log levels across all loggers
+    if (level < lowestLogLevel)
+        return false;
+
     LoggerMap::const_iterator it = loggers.begin();
     while (it != loggers.end() && it->second.getType() != type)
         ++it;
@@ -527,6 +537,7 @@ void Log::LoadFromConfig()
     arenaLogFile = openLogFile("ArenaLogFile", NULL, "a");
     spammLogFile = openLogFile("SpammLogFile", NULL, "a");
     diffLogFile = openLogFile("diffLogFile", NULL, "a");
+    lowestLogLevel = LOG_LEVEL_FATAL;
 }
 
 void Log::outArena(const char * str, ...)
