2d3cd0e3cc7008e90da27aee4bb920270ea9e2aa
cyberbrest <cyberbrest@cyberbrest.com>
2015-07-23 10:08:40 +0300

    [OPT]:  check liquid at login / init set to map.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -22530,7 +22530,7 @@ bool Unit::UpdatePosition(float x, float y, float z, float orientation, bool tel
         UpdateOrientation(orientation);
 
     // code block for underwater state update
-    if (relocated && !m_lastUnderWatterPos.IsInDist(this, World::Relocation_UpdateUnderwateLimit))
+    if (!m_lastUnderWatterPos.IsInDist(this, World::Relocation_UpdateUnderwateLimit))
     {
         m_lastUnderWatterPos = *this;
         UpdateUnderwaterState(GetMap(), x, y, z);
