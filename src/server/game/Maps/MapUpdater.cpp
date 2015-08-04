cabe2aa48f3ef54cf47463714de581d68c9dc999
cyberbrest <cyberbrest@cyberbrest.com>
2015-08-04 17:56:39 +0300

    [Log]: map differ.

--- src/server/game/Maps/MapUpdater.cpp
+++ src/server/game/Maps/MapUpdater.cpp
@@ -39,7 +39,11 @@ class MapUpdateRequest
 
         void call()
         {
+            uint32 diff = getMSTime();
             m_map.Update (m_diff);
+            diff = getMSTime() - diff;
+            if (diff > 40)
+                sLog->outDiff("Map diff: %u. ID %i Players online: %u.", diff, m_map.GetId(), sWorld->GetActiveSessionCount());
             m_updater.update_finished();
         }
 };
