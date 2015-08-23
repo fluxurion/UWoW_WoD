201ca04e549a0a532d1872621eac079eb15c13b3
cyberbrest <d7561985@gmail.com>
2015-08-23 16:03:54 +0300

    [Wod]: save little progress.

--- sql/uwow/2015_06_14_02_world_draenor_intro.sql
+++ sql/uwow/2015_06_14_02_world_draenor_intro.sql
@@ -787,4 +787,10 @@ DELETE FROM smart_scripts WHERE entryorguid = 79917;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (79917, 0, 0, 0, 47, 0, 100, 0, 34925, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'At start q: 34925');
 
--- Q: 34437
+-- Q: 35747
+REPLACE INTO `gossip_menu` (`entry`, `text_id`) VALUES
+(16518, 23994); -- 78568
+REPLACE INTO `npc_text` (`ID`, `text0_0`) VALUES ('23994', 'Мм-м?.. Нужно что-нибудь?');
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `box_coded`, `box_money`, `box_text`) VALUES
+(16518, 1, 0, 'Мне бы хотелось купить что-нибудь у вас.', 0, 0, ''), -- 78568
+(16518, 0, 0, 'Да. Помоги-ка мне управиться с этим огромным танком.', 0, 0, ''); -- 78568
