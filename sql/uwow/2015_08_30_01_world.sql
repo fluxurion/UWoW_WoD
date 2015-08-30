704780a644ddb8e9532a4d8bbdd1bf00d1aea80f
cyberbrest <d7561985@gmail.com>
2015-08-30 17:43:27 +0300

    [SQL]: playercreateinfo_spell add 8386 for go opening for monk

---
+++ sql/uwow/2015_08_30_01_world.sql
@@ -0,0 +1,14 @@
+REPLACE INTO `playercreateinfo_spell` (`race`, `class`, `Spell`, `Note`) VALUES
+(1, 10, 8386, 'Attacking'),
+(2, 10, 8386, 'Attacking'),
+(3, 10, 8386, 'Attacking'),
+(4, 10, 8386, 'Attacking'),
+(5, 10, 8386, 'Attacking'),
+(6, 10, 8386, 'Attacking'),
+(7, 10, 8386, 'Attacking'),
+(8, 10, 8386, 'Attacking'),
+(10, 10, 8386, 'Attacking'),
+(11, 10, 8386, 'Attacking'),
+(24, 10, 8386, 'Attacking'),
+(25, 10, 8386, 'Attacking'),
+(26, 10, 8386, 'Attacking');
