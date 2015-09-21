b04f6f1b193171fb737ec44df7bea65fecdff32b
cyberbrest <d7561985@gmail.com>
2015-09-21 19:14:05 +0300

    [SQL]: goblin fixes.

--- sql/uwow/old/6X/2015_07_15_01_world.sql
+++ sql/uwow/old/6X/2015_07_15_01_world.sql
@@ -1,7 +1,7 @@
 REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (158188, 'spell_rog_nightstalker');
 
-DELETE FROM spell_script_names WHERE spell_script_names = 'spell_rog_nightstalker' AND spell_id = 1784;
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_rog_nightstalker' AND spell_id = 1784;
 
 REPLACE INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `caster`, `target`, `hastalent`, `hastalent2`, `chance`, `cooldown`, `type2`, `hitmask`, `learnspell`, `removeMask`, `comment`) VALUES
  ('1784', '98877', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Stelth - Sanctuary'),
