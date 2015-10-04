980ddd70a2cd75206ee661941775d1ccd079934f
Rubaka <blackmanos@yandex.ru>
2015-10-04 18:52:01 +0300

    тоже самое

--- src/server/game/Movement/PathGenerator.h
+++ src/server/game/Movement/PathGenerator.h
@@ -19,7 +19,7 @@
 #ifndef _PATH_GENERATOR_H
 #define _PATH_GENERATOR_H
 
-#include "Maps\MapDefines.h"
+#include "MapDefines.h"
 #include "DetourNavMesh.h"
 #include "DetourNavMeshQuery.h"
 #include "MoveSplineInitArgs.h"
