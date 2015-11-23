7f66d6fcf68b4a0e28200a55755b7d5b2a57a670
cyberbrest <d7561985@gmail.com>
2015-11-23 13:02:46 +0300

    [SV]:  q34587

--- sql/uwow/2015_11_32_02_world_garrison_alliance_shadowmoon_valley.sql
+++ sql/uwow/2015_11_32_02_world_garrison_alliance_shadowmoon_valley.sql
@@ -104,3 +104,29 @@ DELETE FROM smart_scripts WHERE entryorguid = 81441;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (81441, 0, 0, 0, 62, 0, 100, 0, 16598, 0, 0, 0, 11, 164249, 18, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'At gossip select Q335176 cast'),
 (81441, 0, 1, 0, 62, 0, 100, 0, 16598, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'At gossip select Q335176 say');
+
+-- Q: 34587
+
+DELETE FROM `creature_text` WHERE entry = 77209;
+REPLACE INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
+(77209, 0, 0, 'Благодаря тебе у нас появился отличный форпост в Долине Призрачной Луны.', 12, 0, 100, 396, 0, 0, 'Барос Алекстон to Player'),
+(77209, 1, 0, 'Может, на корабле орков найдется что-нибудь полезное?.. Кроме этих сверл для зубов.', 12, 0, 100, 1, 0, 43529, 'Барос Алекстон to Player'),
+(77209, 2, 0, 'Уф. Может, сегодня мне даже удастся полюбоваться на звезды. Смена обстановки не помешает.', 12, 0, 100, 4, 0, 43533, 'Барос Алекстон to Player'),
+(77209, 3, 0, 'Фу, некоторые припасы совсем промокли и... какие-то липкие. Можно использовать, но выглядят они мерзко.', 12, 0, 100, 1, 0, 43530, 'Барос Алекстон to Player'),
+(77209, 4, 0, 'Казармы мы мигом построим. Командир, следуй за мной.', 12, 0, 100, 0, 0, 42439, 'Барос Алекстон to Player'),
+(77209, 5, 0, 'Строительство других зданий займет гораздо больше времени.', 12, 0, 100, 0, 0, 42439, 'Барос Алекстон to Player'),
+(77209, 6, 0, 'Прими работу, когда пожелаешь.', 12, 0, 100, 0, 0, 42439, 'Барос Алекстон to Player'),
+(77209, 7, 0, 'Казармы выглядят превосходно. Поговори с Мараадом, а я пока полюбуюсь своей искусной работой.', 12, 0, 100, 0, 0, 42439, 'Барос Алекстон to Player');
+
+UPDATE `creature_template` SET  `AIName`='SmartAI' WHERE `entry`=77209;
+DELETE FROM smart_scripts WHERE entryorguid = 77209;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(77209, 0, 0, 0, 47, 0, 100, 0, 34587, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'At gossip at take 34587');
+
+UPDATE `garrison_plot_finalize_info` SET `allianceX` = '1892.78', `allianceY` = '240.9705', `allianceZ` = '76.63956', `allianceO` = '2.789496', `allianceAnimKitId` = '1696' WHERE `garrison_plot_finalize_info`.`garrPlotInstanceId` = 23;
+
+DELETE FROM `garrison_building_gameobject` WHERE map =1158 ;
+INSERT INTO `garrison_building_gameobject` (`plotID`, `BuildID`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`) VALUES
+-- Alliance. Big. empty building
+(23, 0, 231855, 1158, 1906.99, 235.7743, 77.60764, 3.348588, 0, 0, 0, 1);
+
