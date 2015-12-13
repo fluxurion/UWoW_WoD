b0014c820692fcacc3bbc9e0028caf257e374a8d
moJIto-ice <akriso@i.ua>
2015-12-13 17:55:59 +0300

    arena test

--- src/server/game/Battlegrounds/Arena.cpp
+++ src/server/game/Battlegrounds/Arena.cpp
@@ -49,7 +49,7 @@ void Arena::Update(uint32 diff)
     }
     else if (elapsedTime > Minutes(2))
         UpdateWorldState(WorldStates::ARENA_END_TIMER, int32(time(nullptr) + std::chrono::duration_cast<Seconds>(Minutes(25) - elapsedTime).count()));
-    else if (elapsedTime >= Minutes(10))
+    /*else if (elapsedTime >= Minutes(10))
     {
         ModifyStartDelayTime(Milliseconds(diff));
         if (GetStartDelayTime() <= Seconds(0) || GetStartDelayTime() > Seconds(10))
@@ -60,7 +60,7 @@ void Arena::Update(uint32 diff)
                     if (!player->HasAura(SPELL_BG_ARENA_DUMPENING))
                         player->CastSpell(player, SPELL_BG_ARENA_DUMPENING, true);
         }
-    }
+    }*/
 }
 
 inline void Arena::_ProcessJoin(uint32 diff)
@@ -210,14 +210,14 @@ void Arena::RemovePlayerAtLeave(ObjectGuid guid, bool transport, bool sendPacket
 {
     Battleground::RemovePlayerAtLeave(guid, transport, sendPacket);
 
-    Player* player = ObjectAccessor::FindPlayer(guid);
+    /*Player* player = ObjectAccessor::FindPlayer(guid);
     if (!player)
         return;
 
     player->RemoveAurasDueToSpell(SPELL_ARENA_PEREODIC_AURA);
     player->RemoveAurasDueToSpell(SPELL_ENTERING_BATTLEGROUND);
     if (isRated())
-        player->RemoveAurasDueToSpell(SPELL_RATED_PVP_TRANSFORM_SUPPRESSION);
+        player->RemoveAurasDueToSpell(SPELL_RATED_PVP_TRANSFORM_SUPPRESSION);*/
 }
 
 void Arena::CheckWinConditions()
@@ -232,139 +232,136 @@ void Arena::EndBattleground(uint32 winner)
 {
     Battleground::EndBattleground(winner);
 
-    if (!isArena())
-        return;
-
     UpdateWorldState(WorldStates::ARENA_SHOW_END_TIMER, 0);
 
-    bool guildAwarded = false;
-    uint8 aliveWinners = GetAlivePlayersCountByTeam(winner);
-    std::ostringstream info;
-    BracketType bType = BattlegroundMgr::BracketByJoinType(GetJoinType());
+    //bool guildAwarded = false;
+    //uint8 aliveWinners = GetAlivePlayersCountByTeam(winner);
+    //std::ostringstream info;
+    //BracketType bType = BattlegroundMgr::BracketByJoinType(GetJoinType());
 
-    for (BattlegroundPlayerMap::iterator itr = m_Players.begin(); itr != m_Players.end(); ++itr)
-    {
-        uint32 team = itr->second.Team;
+    //for (BattlegroundPlayerMap::iterator itr = m_Players.begin(); itr != m_Players.end(); ++itr)
+    //{
+    //    uint32 team = itr->second.Team;
 
-        if (itr->second.OfflineRemoveTime)
-        {
-            if (isRated() && winner != WINNER_NONE)
-            {
-                Bracket* bracket = sBracketMgr->TryGetOrCreateBracket(itr->first, bType);
-                ASSERT(bracket);    //unreal
-                uint32 gain = bracket->FinishGame(team == winner, GetMatchmakerRating(team == winner ? GetOtherTeam(winner) : winner));
-                info << " >> Plr. Not in game at the end of match. GUID: " << itr->first << " mmr gain: " << gain << " state:" << (team == winner) ? "WINER" : "LOSER";
-            }
-            continue;
-        }
+    //    if (itr->second.OfflineRemoveTime)
+    //    {
+    //        if (isRated() && winner != WINNER_NONE)
+    //        {
+    //            Bracket* bracket = sBracketMgr->TryGetOrCreateBracket(itr->first, bType);
+    //            ASSERT(bracket);    //unreal
+    //            uint32 gain = bracket->FinishGame(team == winner, GetMatchmakerRating(team == winner ? GetOtherTeam(winner) : winner));
+    //            info << " >> Plr. Not in game at the end of match. GUID: " << itr->first << " mmr gain: " << gain << " state:" << (team == winner) ? "WINER" : "LOSER";
+    //        }
+    //        continue;
+    //    }
 
-        Player* player = GetPlayer(itr, "EndBattleground");
-        if (!player)
-            continue;
+    //    Player* player = GetPlayer(itr, "EndBattleground");
+    //    if (!player)
+    //        continue;
 
-        Bracket* bracket = GetJoinType() ? player->getBracket(bType) : nullptr;
-        uint16 bracketRating = bracket->getRating();
-        bool isWinnerTeam = team == winner;
+    //    Bracket* bracket = GetJoinType() ? player->getBracket(bType) : nullptr;
+    //    uint16 bracketRating = bracket->getRating();
+    //    bool isWinnerTeam = team == winner;
 
-        if (isWinnerTeam && isRated() && GetJoinType() == JOIN_TYPE_ARENA_5v5 && aliveWinners == 1 && player->isAlive())
-            player->CastSpell(player, SPELL_BG_THE_LAST_STANDING, true);
+    //    if (isWinnerTeam && isRated() && GetJoinType() == JOIN_TYPE_ARENA_5v5 && aliveWinners == 1 && player->isAlive())
+    //        player->CastSpell(player, SPELL_BG_THE_LAST_STANDING, true);
 
-        if (isRated() && winner != WINNER_NONE)
-        {
-            uint32 gain = bracket->FinishGame(isWinnerTeam, GetMatchmakerRating(isWinnerTeam ? GetOtherTeam(winner) : winner));
-            info << " >> Plr: " << player->ToString().c_str() << " mmr gain: " << gain << " state:" << (isWinnerTeam) ? "WINER" : "LOSER";
-            if (isWinnerTeam)
-            {
-                player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, 1);
-                player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_ARENA, GetMapId());
-                player->ModifyCurrency(CURRENCY_TYPE_CONQUEST_ARENA_META, sWorld->getIntConfig(CONFIG_CURRENCY_CONQUEST_POINTS_ARENA_REWARD));
+    //    if (isRated() && winner != WINNER_NONE)
+    //    {
+    //        uint32 gain = bracket->FinishGame(isWinnerTeam, GetMatchmakerRating(isWinnerTeam ? GetOtherTeam(winner) : winner));
+    //        info << " >> Plr: " << player->ToString().c_str() << " mmr gain: " << gain << " state:" << (isWinnerTeam) ? "WINER" : "LOSER";
+    //        if (isWinnerTeam)
+    //        {
+    //            player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, 1);
+    //            player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_ARENA, GetMapId());
+    //            player->ModifyCurrency(CURRENCY_TYPE_CONQUEST_ARENA_META, sWorld->getIntConfig(CONFIG_CURRENCY_CONQUEST_POINTS_ARENA_REWARD));
 
-                _arenaTeamScores[team].Assign(bracketRating, bracketRating + gain, bracket->getLastMMRChange());
+    //            _arenaTeamScores[team].Assign(bracketRating, bracketRating + gain, bracket->getLastMMRChange());
 
-                if (!guildAwarded)
-                {
-                    guildAwarded = true;
-                    if (ObjectGuid::LowType guildId = GetBgMap()->GetOwnerGuildId(player->GetTeam()))
-                        if (Guild* guild = sGuildMgr->GetGuildById(guildId))
-                            guild->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, std::max<uint32>(bracketRating, 1), 0, 0, nullptr, player);
-                }
-            }
-            else
-            {
-                player->GetAchievementMgr().ResetAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, ACHIEVEMENT_CRITERIA_CONDITION_NO_LOSE);
-                _arenaTeamScores[team].Assign(bracketRating, bracketRating - gain, bracket->getLastMMRChange());
-            }
+    //            if (!guildAwarded)
+    //            {
+    //                guildAwarded = true;
+    //                if (ObjectGuid::LowType guildId = GetBgMap()->GetOwnerGuildId(player->GetTeam()))
+    //                    if (Guild* guild = sGuildMgr->GetGuildById(guildId))
+    //                        guild->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, std::max<uint32>(bracketRating, 1), 0, 0, nullptr, player);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            player->GetAchievementMgr().ResetAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_RATED_ARENA, ACHIEVEMENT_CRITERIA_CONDITION_NO_LOSE);
+    //            _arenaTeamScores[team].Assign(bracketRating, bracketRating - gain, bracket->getLastMMRChange());
+    //        }
 
-            player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_PLAY_ARENA, GetMapId());
-        }
-        else if (isRated())
-        {
-            _arenaTeamScores[TEAM_ALLIANCE].Assign(bracketRating, bracketRating - 16, bracket->getLastMMRChange());
-            _arenaTeamScores[TEAM_HORDE].Assign(bracketRating, bracketRating - 16, bracket->getLastMMRChange());
+    //        player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_PLAY_ARENA, GetMapId());
+    //    }
+    //    else if (isRated())
+    //    {
+    //        _arenaTeamScores[TEAM_ALLIANCE].Assign(bracketRating, bracketRating - 16, bracket->getLastMMRChange());
+    //        _arenaTeamScores[TEAM_HORDE].Assign(bracketRating, bracketRating - 16, bracket->getLastMMRChange());
 
-            bracket->FinishGame(false, -16);
-        }
-    }
+    //        bracket->FinishGame(false, -16);
+    //    }
+    //}
 
-    if (isRated() && winner != WINNER_NONE)
-    {
-        const char* winnerTeam[5];
-        const char*  loserTeam[5];
-        for (uint8 i = 0; i < 5; ++i)
-        {
-            winnerTeam[i] = "";
-            loserTeam[i] = "";
-        }
-        uint8 w = 0;
-        uint8 l = 0;
+    //if (isRated() && winner != WINNER_NONE)
+    //{
+    //    const char* winnerTeam[5];
+    //    const char*  loserTeam[5];
+    //    for (uint8 i = 0; i < 5; ++i)
+    //    {
+    //        winnerTeam[i] = "";
+    //        loserTeam[i] = "";
+    //    }
+    //    uint8 w = 0;
+    //    uint8 l = 0;
 
-        for (std::list<const char*>::iterator itr = m_nameList[winner].begin(); itr != m_nameList[winner].end(); ++itr)
-            if (const char* winnerName = (*itr))
-            {
-                winnerTeam[w] = winnerName;
-                w++;
-            }
+    //    for (std::list<const char*>::iterator itr = m_nameList[winner].begin(); itr != m_nameList[winner].end(); ++itr)
+    //        if (const char* winnerName = (*itr))
+    //        {
+    //            winnerTeam[w] = winnerName;
+    //            w++;
+    //        }
 
-        for (std::list<const char*>::iterator itr = m_nameList[GetOtherTeam(winner)].begin(); itr != m_nameList[GetOtherTeam(winner)].end(); ++itr)
-            if (const char* loserName = (*itr))
-            {
-                loserTeam[l] = loserName;
-                l++;
-            }
+    //    for (std::list<const char*>::iterator itr = m_nameList[GetOtherTeam(winner)].begin(); itr != m_nameList[GetOtherTeam(winner)].end(); ++itr)
+    //        if (const char* loserName = (*itr))
+    //        {
+    //            loserTeam[l] = loserName;
+    //            l++;
+    //        }
 
-        uint32 _arenaTimer = GetElapsedTime().count();
-        uint32 _min = _arenaTimer / 60;
-        uint32 _sec = _arenaTimer - (_min * 60);
-        const char* att = "";
-        if (GetElapsedTime() < Seconds(75))
-            att = "--- ATTENTION!";
+    //    uint32 _arenaTimer = GetElapsedTime().count();
+    //    uint32 _min = _arenaTimer / 60;
+    //    uint32 _sec = _arenaTimer - (_min * 60);
+    //    const char* att = "";
+    //    if (GetElapsedTime() < Seconds(75))
+    //        att = "--- ATTENTION!";
 
-        switch (m_JoinType)
-        {
-            case JOIN_TYPE_ARENA_2v2:
-            {
-                sLog->outArena("FINISH: Arena match Type: 2v2 --- Winner[%s, %s]: old rating: %u --- Loser[%s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
-                               winnerTeam[0], winnerTeam[1], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
-                break;
-            }
-            case JOIN_TYPE_ARENA_3v3:
-            {
-                sLog->outArena("FINISH: Arena match Type: 3v3 --- Winner[%s, %s, %s]: old rating: %u --- Loser[%s, %s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
-                               winnerTeam[0], winnerTeam[1], winnerTeam[2], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], loserTeam[2], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
-                break;
-            }
-            case JOIN_TYPE_ARENA_5v5:
-            {
-                sLog->outArena("FINISH: Arena match Type: 5v5 --- Winner[%s, %s, %s, %s, %s]: old rating: %u --- Loser[%s, %s, %s, %s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
-                               winnerTeam[0], winnerTeam[1], winnerTeam[2], winnerTeam[3], winnerTeam[4], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], loserTeam[2], loserTeam[3], loserTeam[4], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
-                break;
-            }
-            default:
-                sLog->outArena("match Type: %u --- Winner: old rating: %u  --- Loser: old rating: %u| DETAIL: %s",
-                               m_JoinType, GetMatchmakerRating(winner), GetMatchmakerRating(GetOtherTeam(winner)), info.str().c_str());
-                break;
-        }
-    }
+    //    switch (m_JoinType)
+    //    {
+    //        case JOIN_TYPE_ARENA_2v2:
+    //        {
+    //            sLog->outArena("FINISH: Arena match Type: 2v2 --- Winner[%s, %s]: old rating: %u --- Loser[%s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
+    //                           winnerTeam[0], winnerTeam[1], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
+    //            break;
+    //        }
+    //        case JOIN_TYPE_ARENA_3v3:
+    //        {
+    //            sLog->outArena("FINISH: Arena match Type: 3v3 --- Winner[%s, %s, %s]: old rating: %u --- Loser[%s, %s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
+    //                           winnerTeam[0], winnerTeam[1], winnerTeam[2], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], loserTeam[2], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
+    //            break;
+    //        }
+    //        case JOIN_TYPE_ARENA_5v5:
+    //        {
+    //            sLog->outArena("FINISH: Arena match Type: 5v5 --- Winner[%s, %s, %s, %s, %s]: old rating: %u --- Loser[%s, %s, %s, %s, %s]: old rating: %u --- Duration: %u min. %u sec. %s",
+    //                           winnerTeam[0], winnerTeam[1], winnerTeam[2], winnerTeam[3], winnerTeam[4], GetMatchmakerRating(winner), loserTeam[0], loserTeam[1], loserTeam[2], loserTeam[3], loserTeam[4], GetMatchmakerRating(GetOtherTeam(winner)), _min, _sec, att);
+    //            break;
+    //        }
+    //        default:
+    //            sLog->outArena("match Type: %u --- Winner: old rating: %u  --- Loser: old rating: %u| DETAIL: %s",
+    //                           m_JoinType, GetMatchmakerRating(winner), GetMatchmakerRating(GetOtherTeam(winner)), info.str().c_str());
+    //            break;
+    //    }
+    //}
 }
 
 void Arena::SendOpponentSpecialization(uint32 team)
